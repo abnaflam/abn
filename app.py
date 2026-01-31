9531cb1a0af90f6b31c58ee336f492ce61b13118
abnaflam <ravynerinnn@gmail.com>
2026-01-31 12:20:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
-from flask import Flask, Response
+import os
 import requests
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-SOURCE_M3U8 = "https://kid-3bk.pages.dev/mbein/bein1.m3u8"
+SOURCE = "https://kid-3bk.pages.dev/mbein/bein1.m3u8"
 
 @app.route("/bein1.m3u8")
-def restream():
-    headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Referer": "https://kid-3bk.pages.dev/"
-    }
-
-    r = requests.get(SOURCE_M3U8, headers=headers, stream=True, timeout=10)
-
+def stream():
+    r = requests.get(SOURCE, stream=True)
     return Response(
-        r.iter_content(chunk_size=1024),
+        r.iter_content(1024),
         content_type="application/vnd.apple.mpegurl"
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
